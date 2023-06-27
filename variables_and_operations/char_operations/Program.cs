b7827b4ca3f6e5789d94f862d66f36c4746421aa
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-27 23:23:34 +0300

    Po pamokos

---
+++ variables_and_operations/char_operations/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace char_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol1 = 'a';
+            char symbol2 = 'b';
+            char symbol3 = 'c';
+
+            string connectedSymbol1 = $"{symbol1}{symbol2}{symbol3}";
+            string connectedSymbols2 = 
+                symbol1.ToString() + 
+                symbol2.ToString() + 
+                symbol3.ToString();
+
+            string connectedSymbols3 = string.Format(
+                "{0}{1}{2}", 
+                symbol1, 
+                symbol2, 
+                symbol3
+            );
+        }
+    }
+}
