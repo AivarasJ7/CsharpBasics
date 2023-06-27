6640f123cfabac691df8ec601b0a5568e6443576
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-27 23:31:03 +0300

    test

---
+++ csharp1/csharp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace csharp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vienas assignment -vienas solution, vienas merge request.
+            //Susikurkite C# programą, kurioje aprašykite kintamuosius kode. Pagal reikalavimus (Code convention).
+            //Vardas;
+            //Pavardė;
+            //Amžius;
+            //Ūgis;
+            //Svoris;
+            //Aukštoji mokykla;
+            //Akademinės grupės kodas;
+            //Kursas;
+            //Studijų programos pavadinimas;
+            //Atsiskaitytų kreditų skaičius;
+        }
+    }
+}
