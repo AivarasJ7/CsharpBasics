c1dfc49986f90b0a82232be4ad368084601682f0
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-29 15:37:54 +0300

    assignment1

---
+++ csharp2/assignment1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aivaras";
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
---
+++ csharp2/csharp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection.Emit;
+using System;
+
+namespace csharp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Kiekvieną užduotėlę žemiau kurkite tame pačiame solution.Kiekviena užduotėlė atskirame projekte.
+            Kiekvienai užduočiai(assignment) darykite atskirą commit.Merge request visam šitam assignment vienas.
+            1.Parašykite programą, kuri išvestų žodį “Labas” ir jūsų vardą toje pačioje eilutėje. +
+            2.Susikurkite kintamąjį žmogaus amžiui nusakyti, išveskite į ekraną tekstą su šiuo amžiumi: “Įvestas amžius: { amžius}”. Tarp { }
+            nurodytas kintamasis, kurio reikšmė toje vietoje turi būti įstatyta.
+            3.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje be tarpų tarp šių skaičių(skaičius - 25, išvedimas - 2525252525).
+            4.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje su tarpais tarp šių skaičių(skaičius - 25, rezultatas - 25 25 25 25 25).
+            5.Išveskite į ekraną norimo dydžio tuščiavidurį kvadratą iš žvaigždučių.
+            ****
+            * *
+            * *
+            ****
+            6.Susikurkite reikiamus kintamuosius užduočiai atlikti. Į ekraną išveskite:
+            Automobilio { markė}
+            { modelis} ({ metai}
+            m.) rida - { rida}
+            km.
+            Tarp { }
+            nurodyti reikiami kintamieji, šiose vietose jų reikšmės turi būti įsistatytos.*/
+
+        }
+    }
+}
