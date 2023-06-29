287b6f40556669c99bdc984148b41425abb5191a
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-29 19:16:57 +0300

    assignment5

---
+++ csharp2/assignment5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****");
+            Console.WriteLine("*  *");
+            Console.WriteLine("*  *");
+            Console.WriteLine("****");
+
+        }
+    }
+}
--- csharp2/csharp2/Program.cs
+++ csharp2/csharp2/Program.cs
@@ -15,7 +15,7 @@ namespace csharp2
             nurodytas kintamasis, kurio reikšmė toje vietoje turi būti įstatyta. +
             3.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje be tarpų tarp šių skaičių(skaičius - 25, išvedimas - 2525252525). +
             4.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje su tarpais tarp šių skaičių(skaičius - 25, rezultatas - 25 25 25 25 25). +
-            5.Išveskite į ekraną norimo dydžio tuščiavidurį kvadratą iš žvaigždučių.
+            5.Išveskite į ekraną norimo dydžio tuščiavidurį kvadratą iš žvaigždučių. +
             ****
             * *
             * *
