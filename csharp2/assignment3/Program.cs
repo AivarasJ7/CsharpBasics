4d116ed57fa566abcb7b5e7d140a9f3d5cd6fa0d
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-29 19:08:12 +0300

    assignment3

---
+++ csharp2/assignment3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 25;
+            Console.Write($"{number}{number}{number}{number}{number}");
+        }
+    }
+}
