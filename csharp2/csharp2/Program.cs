b9f071ea7d251c7de835275f7460d8be0f15e9e2
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-29 19:12:03 +0300

    assignment4

--- csharp2/csharp2/Program.cs
+++ csharp2/csharp2/Program.cs
@@ -13,8 +13,8 @@ namespace csharp2
             1.Parašykite programą, kuri išvestų žodį “Labas” ir jūsų vardą toje pačioje eilutėje. +
             2.Susikurkite kintamąjį žmogaus amžiui nusakyti, išveskite į ekraną tekstą su šiuo amžiumi: “Įvestas amžius: { amžius}”. Tarp { }
             nurodytas kintamasis, kurio reikšmė toje vietoje turi būti įstatyta. +
-            3.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje be tarpų tarp šių skaičių(skaičius - 25, išvedimas - 2525252525).
-            4.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje su tarpais tarp šių skaičių(skaičius - 25, rezultatas - 25 25 25 25 25).
+            3.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje be tarpų tarp šių skaičių(skaičius - 25, išvedimas - 2525252525). +
+            4.Susikurkite skaičiaus kintamąjį, kurį išveskite penkis kartus toje pačioje eilutėje su tarpais tarp šių skaičių(skaičius - 25, rezultatas - 25 25 25 25 25). +
             5.Išveskite į ekraną norimo dydžio tuščiavidurį kvadratą iš žvaigždučių.
             ****
             * *
