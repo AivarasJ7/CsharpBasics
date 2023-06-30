3167559e1ea933fef2d8800fe0149d208720807d
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-06-30 21:56:04 +0300

    assignment2

---
+++ csharp3/assignment2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Išveskite į ekraną pasirinktą skaičių, jo kvadratą ir jį pakeltą trečiuoju laipsniu.
+
+            int num1 = 5;
+            int square = num1 * num1;
+            int thirdpower = num1 * num1 * num1;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(square);
+            Console.WriteLine(thirdpower);
+
+            
+        }
+    }
+}
--- csharp3/csharp3/Program.cs
+++ csharp3/csharp3/Program.cs
@@ -16,7 +16,7 @@
             int sum = num1 + num2;
             int difference = num1 - num2;
             int multiplication = num1 * num2;
-            double division = (double)num1 / num2;
+            decimal division = (decimal)num1 / num2;
 
             Console.WriteLine("Suma: {0} + {1} = {2}", num1, num2, sum);
             Console.WriteLine("Skirtumas: {0} - {1} = {2}", num1, num2, difference);
