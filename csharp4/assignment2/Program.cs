29a72df1ac2f8267cbfc218c6e1be4d1d7eeb338
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-01 18:16:12 +0300

    assignment2

---
+++ csharp4/assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Įveskite simbolį: ");
+            char symbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{symbol}{symbol}{symbol}");
+            Console.WriteLine($"{symbol}{symbol}{symbol}");
+            Console.WriteLine($"{symbol}{symbol}{symbol}");
+        }
+    }
+}
