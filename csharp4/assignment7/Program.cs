df5dbee6d3f1cc5dba1e5d40b0fbdcbf709732ce
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-01 19:27:21 +0300

    assignment7

---
+++ csharp4/assignment7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*7. Parašykite programą, kuri laipsnius paverstų į kelvinus ir fahrenheitus.
+                 (0°C × 9/5) + 32 = 32°F
+                 0°C + 273.15 = 273.15K
+             */
+
+            Console.Write("Įveskite temperatūrą °C:");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            double kelvin = celsius + 273.15;
+
+            Console.WriteLine("Temperatūra °F: " + fahrenheit);
+            Console.WriteLine("Temperatūra K: " + kelvin);
+        }
+    }
+}
