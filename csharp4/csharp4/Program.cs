7a5499788a9bc0256ddae9a22346ef21a1e7d345
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-01 18:09:15 +0300

    assignment1

---
+++ csharp4/csharp4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace csharp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Liepkite vartotojui įvesti savo vardą ir amžių. Į ekraną išveskite:
+                “Jūsų vardas { vardas},  o amžius { amžius}.” */
+
+            Console.Write("Įveskite savo vardą: ");
+            string name = Console.ReadLine();
+
+            string ageInput;
+            int age;
+
+            do
+            {
+                Console.Write("Įveskite savo amžių: ");
+                ageInput = Console.ReadLine();
+
+                if (!int.TryParse(ageInput, out age))
+                {
+                    Console.WriteLine("Neteisingai įvestas amžius. Įveskite iš naujo.");
+                }
+            } while (!int.TryParse(ageInput, out age));
+
+            Console.WriteLine($"Jūsų vardas {name}, o amžius {age}.");
+        }
+    }
+}
