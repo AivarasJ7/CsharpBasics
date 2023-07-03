1ec8d6107e52888690d0c43610750661fbb5f88d
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-03 23:19:22 +0300

    atlikus uzduoti

---
+++ csharp5/csharp5_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace csharp5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Visose užduotyse turi išspausdinti true arba false ekrane.
+            //Pvz. "Ar duotas skaičius yra lyginis ir didesnis nei 10? atsakymas: true"
+            //Užduotis 3: Patikrinkite, ar vartotojo amžius yra tarp 18 ir 65 metų imtinai.
+            // [18, 65]
+
+            Console.WriteLine("Iveskite savo amziu: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(age >= 18 && age <= 65);
+        }
+    }
+}
