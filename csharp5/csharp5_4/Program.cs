50c4a84cea5a0157fb6fa0800509d3db49cf8659
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-03 23:28:41 +0300

    atlikus uzduoti

---
+++ csharp5/csharp5_4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace csharp5_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Visose užduotyse turi išspausdinti true arba false ekrane. Pvz.
+            //"Ar duotas skaičius yra lyginis ir didesnis nei 10? atsakymas: true"
+            //Užduotis 4: Patikrinkite, ar vartotojas turi teisę gauti nuolaidą, jei nuolaida galioja studentams arba senjorams.
+            // studentas (18, 25] senjoras > 65
+
+            Console.WriteLine("Iveskite savo amziu: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(age > 18 && age <= 25 || age > 65);
+        }
+    }
+}
