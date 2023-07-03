98a2b735ff23fc4053f0c38b9174463084b2a5d6
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-03 22:54:38 +0300

    po paskaitos2

---
+++ logical_actions/if_else/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // isspausdinti ar skaicius yra lyginis ar nelyginis
+            Random random = new Random();
+            int number = random.Next(1, 100);
+            bool isEvenNumber = number % 2 == 0;
+
+            if (isEvenNumber)
+            {
+                Console.WriteLine($"Skaicius {number} yra lyginis");
+            }
+            else
+            {
+                Console.WriteLine($"Skaicius {number} yra nelyginis");
+            }
+
+            string password = "qwerty123";
+
+            if (password == "qwerty123" ||
+               password == "zxcvb123" ||
+               password == "asdfgh123")
+            {
+                Console.WriteLine("Valio! Nulauzta");
+            }
+            else
+            {
+                Console.WriteLine("Deja, Nepavyko");
+            }
+
+            // ar atsitiktinis skaicius yra intervale [5, 50]
+
+            if (number >= 5 && number <= 50)
+            {
+                Console.WriteLine($"skaicius {number} yra intervale [5, 50]");
+            }
+            else 
+            {
+                Console.WriteLine($"skaicius {number} nera intervale [5, 50]");
+            }
+        }
+    }
+}
---
+++ logical_actions/logical_operations/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace logical_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //veiksmai su palyginimu
+            Console.WriteLine(5 < 6);
+
+            bool result1 = 10 == 10; 
+
+            Console.WriteLine(result1);
+
+            int age1 = 10;
+            int age2 = 15;
+
+            Console.WriteLine(age1 < age2);
+
+            string password1 = "Qwerty123";
+            string password2 = "qwerty123";
+
+            Console.WriteLine(password1 == password2);
+            Console.WriteLine(password1 != password2);
+            Console.WriteLine(password1.Length >= password2.Length);
+
+            int age = 20;
+
+            //patikrite ar amzius yra didesnis uz 20;
+            Console.WriteLine(age > 20);
+
+            //patikrite ar amzius yra didesnis arba lygus uz 20;
+            Console.WriteLine(age >= 20);
+
+            // [5, 10]
+            // ] - imtinai
+            // ) - ne imtinai
+
+            // [5, 10) -- intervalas
+            // AND &&
+            // OR ||
+
+            int x = 6;
+
+            Console.WriteLine(x >= 5 && x < 10); // AND atveju kai visi true, bendras true;
+
+            x = 100;
+
+            Console.WriteLine(x >= 5 || x < 10); // OR atveju bent vienas yra true, visi true;
+
+            char symbol1 = 'a';
+
+            Console.WriteLine(symbol1 == 'A' || symbol1 == 'a');
+
+            string password = "qwerty";
+
+            Console.WriteLine(
+                password == "qwerty" ||
+                password == "QWERTY" ||
+                password == "Qwerty" ||
+                password == "QwErty"
+            );
+        }
+    }
+}
