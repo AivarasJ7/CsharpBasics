2c343782a5f6af3a2adf035e0159f3b0ec94d39b
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-03 23:06:24 +0300

    atlikus uzduoti

---
+++ csharp5/chsharp5_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace chsharp5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Visose užduotyse turi išspausdinti true arba false ekrane. Pvz.
+             "Ar duotas skaičius yra lyginis ir didesnis nei 10? atsakymas: true"
+              Užduotis 1: Patikrinkite, ar duotas skaičius yra lyginis ir didesnis nei 10.
+             */
+
+            Random random = new Random();
+            int number = random.Next(0, 100);
+
+            Console.WriteLine(number);
+            Console.WriteLine(number % 2 == 0 && number > 10);
+        }
+    }
+}
