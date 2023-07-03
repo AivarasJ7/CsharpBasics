b870c828bb1cf94fa8109607da479901baabe9f6
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-03 23:35:29 +0300

    atlikus uzduoti

--- csharp5/csharp5_5/Program.cs
+++ csharp5/csharp5_5/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Visose užduotyse turi išspausdinti true arba false ekrane. Pvz.
+            //"Ar duotas skaičius yra lyginis ir didesnis nei 10? atsakymas: true"
+            //Užduotis 5: Nustatykite, ar duotas skaičius yra neigiamas 
+            //ir ar jo liekana, dalijant iš 3, yra lygi 2.
+
+            Random random = new Random();
+            int number = random.Next(-50, 51);
+
+            Console.WriteLine(number);
+
+            Console.WriteLine(
+                number < 0 &&
+                (number % 3) % 2 == 0
+            );
         }
     }
 }
