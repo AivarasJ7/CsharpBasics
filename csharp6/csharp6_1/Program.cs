16f169601c6cfb1f3b683a6c6552d4293665a588
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-04 22:54:34 +0300

    assignment1

---
+++ csharp6/csharp6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace csharp6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Naudojant if sąlygas:
+            //Užduotis 1: Patikrinti, ar skaičius yra didesnis nei 50.
+
+            Random random = new Random();
+            int number = random.Next(1, 101);
+
+            Console.WriteLine(number);
+            Console.WriteLine(number > 50);
+        }
+    }
+}
