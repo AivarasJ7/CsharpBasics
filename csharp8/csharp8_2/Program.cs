a7624d4bfb41c841be64661e1b8626d108f74eb7
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-05 18:21:41 +0300

    8.2

---
+++ csharp8/csharp8_2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace csharp8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Liepkite vartotojui įvesti vieną norimą skaičių. Su viena if sąlyga (ir visomis jos dalimis), patikrinkite šiuos dalykus:
+            //2.1.ar skaičius neigiamas;
+            //2.2.ar skaičius teigiamas;
+            //2.3.ar skaičius lygus 0.
+
+            Console.WriteLine("Iveskite skaiciu: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine($"Skaicius '{number}' yra neigiamas.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"Skaicius '{number}' yra teigiamas.");
+            }
+            else
+            {
+                Console.WriteLine("Skaičius yra lygus 0");
+            }
+
+        }
+    }
+}
