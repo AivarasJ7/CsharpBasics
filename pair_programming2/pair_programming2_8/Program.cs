13a9d79deca37253095169ff5d3548e6cfe3d149
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-06 23:54:12 +0300

    pair

---
+++ pair_programming2/pair_programming2_8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pair_programming2_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8. Raskite visų skaičių nuo 1 iki 100 sumą. (Ats.4950 )
+
+            int result = 0;
+
+            for (int i = 1; i < 100; i++)
+            {
+                result = result + i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ pair_programming2/pair_programming2_9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pair_programming2_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //9. Raskite visų lyginių skaičių nuo 20 iki 50 sumą. (Ats.510 )
+
+            int result = 0;
+
+            for (int i = 20; i < 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += i;
+                }
+                
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
