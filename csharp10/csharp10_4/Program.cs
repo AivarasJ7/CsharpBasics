bc5b34bfd7814ec0d0452734645ffa762cd143d1
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-07 22:44:51 +0300

    csharp10_4

---
+++ csharp10/csharp10_4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace csharp10_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4.Sukurkite string tipo kintamąjį.Prie jo vis pridėkite vartotojo įrašomus skirtingus žodžius. 
+            // Tačiau jei vartotojas įrašytų “exit” -baigti ciklą.Baigus vykdyti ciklą turi būti išvestas string kintamasis prie kurio vis buvo pridedami skirtingi žodžiai. 
+
+            string result = string.Empty;
+            string input;
+
+            while (true)
+            {
+                Console.Write("Iveskite zodi arba exit: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                    break;
+
+                result += input + " ";
+            }
+
+            Console.WriteLine($"Rezultatas: {result} ");
+        }
+    }
+}
