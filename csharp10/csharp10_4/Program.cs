6e7d68abb56090bb4c6db543a10e29eddf333a25
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-07 22:57:47 +0300

    csharp10_5

--- csharp10/csharp10_4/Program.cs
+++ csharp10/csharp10_4/Program.cs
@@ -6,6 +6,7 @@
         {
             // 4.Sukurkite string tipo kintamąjį.Prie jo vis pridėkite vartotojo įrašomus skirtingus žodžius. 
             // Tačiau jei vartotojas įrašytų “exit” -baigti ciklą.Baigus vykdyti ciklą turi būti išvestas string kintamasis prie kurio vis buvo pridedami skirtingi žodžiai. 
+            // (pridėtas paveiksliukas 4.jpg)
 
             string result = string.Empty;
             string input;
---
+++ csharp10/csharp10_5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace csharp10_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5. Liepkite vartotojui įvedinėti atsitiktinius skaičius. Šį ciklą vykdyti tol, kol įvestas skaičius nesidalins iš 9. 
+            //(pridėtas paveiksliukas 5.jpg)
+
+            int number;
+
+            do
+            {
+                Console.WriteLine("Iveskite bet koki skaiciu: ");
+                number = int.Parse(Console.ReadLine());
+
+            } while (number % 9 != 0);
+
+            Console.WriteLine($"{number} yra skaicius kuris dalinasi is 9");
+        }
+    }
+}
