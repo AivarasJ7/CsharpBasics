c785edb32e8ef8a3095e2d142d33464fc759a6e3
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-12 14:04:50 +0300

    csharp12

---
+++ csharp12/csharp12_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace csharp12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Susikurti šiuos masyvus su duomenimis:
+            // 1.1.skirtą studentų pažymiams saugoti;
+
+            int[] studentGrades = { 9, 7, 8, 6, 5, 6, 9, 10, 9, 10, 6, 8, 5, 4, 8, 8, 7, 10, 10, 8 };
+
+            // 1.2.skirtą mašinų markėms saugoti;
+
+            string[] carBrands = { "Ford", "BMW", "Audi", "Opel", "Dodge", "Toyota" };
+
+            // 1.3.savo norimą.
+
+            string[] cardSuits = { "spades", "clubs", "hearts", "diamonds " };
+
+            // 2.Naudojant foreach cikla išspausdinti iš pirmojo projekto pasirinktą masyvą. 
+            // Kiekvieną elementą spausdinant i atskirą eilutę.
+
+            foreach (string suit in cardSuits)
+            {
+                Console.WriteLine(suit);
+            }
+
+            // 3. Naudojant foreach cikla išspausdinti kitą iš pirmojo projekto pasirinktą. Grupuoti elementus po 5 per eilute.
+
+            int count = 0;
+
+            foreach (int grade in studentGrades)
+            {
+                Console.Write($"{grade}, ");
+                count++;
+                if (count % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
