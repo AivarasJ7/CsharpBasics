69fdb11c33b065da341f70a1c45e4255b99bc830
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-12 17:18:44 +0300

    csharp13_3

---
+++ csharp13/csharp13_3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace csharp13_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. Susikurkite kintamąjį, kuris pasakytų kokia yra automobilio bako talpa.
+
+            int fuelTank = 65;
+
+            // Susikurkite masyvą, kuriame saugotumėte skirtingose kelionėse nuvažiuotų kilometrų rodmenis.
+            // Masyvo elementų skaičius tai kartai kiek automobilis gali nuvažiuoti su pilnu baku.
+
+            double[] trips = { 320, 270, 100 };
+
+            //Raskite:
+            // 3.1.trumpiausią kelionę;
+
+            Console.Write("Trumpiausia kelione: ");
+            Console.WriteLine(trips.Min());
+
+            // 3.2.ilgiausią kelionę;
+
+            Console.Write("Ilgiausia kelione: ");
+            Console.WriteLine(trips.Max());
+
+            // 3.3.kiek vidutiniškai kuro sąnaudų sunaudojo automobilis kiekvienos kelionės metu;
+
+            double totalFuelConsumption = trips.Sum();
+            double averageFuelConsumption = totalFuelConsumption / fuelTank;
+
+            Console.Write("Vidutiniškai kuro sąnaudų sunaudojo automobilis kiekvienos kelionės metu: ");
+            Console.WriteLine(averageFuelConsumption);
+        }
+    }
+}
