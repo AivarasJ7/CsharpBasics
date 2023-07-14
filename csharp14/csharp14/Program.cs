50d23193e1b338a5c09178113c4b5bb88c113090
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-14 12:52:01 +0300

    csharp14

---
+++ csharp14/csharp14/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace csharp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Užduotis 1: Sukurkite dvimatį masyvą, kuriame bus saugomi studentų pažymiai kiekvienam dalykui. 
+            // Apskaičiuokite visų pažymių sumą ir vidurkį.
+
+            int[,] studentGrades = new int[,]
+            {
+                {10, 5, 8, 7, 6 },
+                {9, 7, 4, 3, 7},
+                {5, 8, 7, 4, 10}
+            };
+
+            int sum = 0;
+            int count = 0;
+
+            for (int row = 0; row < studentGrades.GetLength(0); row++) // Nezinau ar gerai cia tie (row, col) bet labai man aiskiai matosi kas kaip vaiksto. Siaip pamenu sakei raideles turi buti i ir j? berods.
+            {
+                for (int col = 0; col < studentGrades.GetLength(1); col++)
+                {
+                    sum += studentGrades[row, col];
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {(double)sum / count}");
+        }
+    }
+}
