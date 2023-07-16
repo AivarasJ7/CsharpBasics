1faa60672387d5da571a69249ac6278910897e8b
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-16 13:09:26 +0300

    fix

--- csharp16/csharp16/Program.cs
+++ csharp16/csharp16/Program.cs
@@ -23,44 +23,6 @@ namespace csharp16
             {
                 Console.WriteLine(name);
             }
-
-            //2. Parašykite programą, kuri leidžia vartotojui įvesti n sveikųjų skaičių į List<int> sąrašą, 
-            // o tada randa ir atspausdina didžiausią ir mažiausią skaičių.
-
-            List<int> numbers = new List<int>();
-
-            Console.WriteLine("Iveskite elementu skaiciu: ");
-            int count = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < count; i++)
-            {
-                Console.Write($"Iveskite skaiciu #{i + 1}: ");
-                int number = int.Parse(Console.ReadLine());
-                numbers.Add(number);
-            }
-
-            if (numbers.Count > 0)
-            {
-                int largest = numbers[0];
-                int smallest = numbers[0];
-
-                foreach (int number in numbers)
-                {
-                    if (number > largest)
-                        largest = number;
-
-                    if (number < smallest)
-                        smallest = number;
-                }
-
-                Console.WriteLine($"Didziausias skaicius: {largest}");
-                Console.WriteLine($"Maziausias skaicius: {smallest}");
-            }
-
-            else
-            {
-                Console.WriteLine("Neteisingai pasirinktas elementu skaicius.");
-            }
         }
     }
 }
