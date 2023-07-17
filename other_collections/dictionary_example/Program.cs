07af5d32c90b9efc849c700823ecf7a424fa373b
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-17 22:32:57 +0300

    po paskaitos

---
+++ other_collections/dictionary_example/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace dictionary_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> studentsDictionary = new Dictionary<int, string>()
+            {
+                {1, "Jonas"},
+                {2, "Petras"},
+                {3, "Algis"}
+            };
+
+            Console.WriteLine($"Studentas pagal rakta: {studentsDictionary[2]}");
+
+            if (studentsDictionary.TryGetValue(1, out string outedValue))
+            {
+                Console.WriteLine($"Radau studenta: {outedValue}");
+            }
+
+            foreach (int key in studentsDictionary.Keys)
+            {
+                Console.WriteLine($"Studentas pagal rakta: {key} - {studentsDictionary[key]}");
+            }
+
+            // papildity zodyna
+            studentsDictionary.Add(4, "Ona");
+
+            //bandymas prideti ir informavimas kad nepavyko
+
+            if (!studentsDictionary.TryAdd(4, "Ona"))
+            {
+                Console.WriteLine("Deja toks raktas jau yra zodyne");
+            }
+
+            //pasalina elementa ir grazina bool reiksme true - pasalina, false- nepasalina
+            if (studentsDictionary.Remove(4))
+            {
+                Console.WriteLine("pavyko istrinti");
+            }
+            else
+            {
+                Console.WriteLine("nepavyko istrinti");
+            }
+
+            if (studentsDictionary.ContainsKey(2)) 
+            {
+                Console.WriteLine($"Visdar nepasalintas {studentsDictionary[2]}");
+            }
+        }
+    }
+}
---
+++ other_collections/hashset_example/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace hashset_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // hashsetas saugo tik unikalias reiksmes
+
+            HashSet<int> uniqueIds = new HashSet<int>();
+
+            uniqueIds.Add(1);
+            uniqueIds.Add(2);
+            uniqueIds.Add(3);
+            uniqueIds.Add(1); // jei pridesime jau esancia reiksme klaidos nebus. parprasciau neprides jos i hashseta
+
+            foreach (int id in uniqueIds)
+            {
+                Console.WriteLine($"Id: {id}");
+            }
+
+            Console.WriteLine($"Elementu hashset rinkynyje yra: {uniqueIds.Count}"); // be .Count() butinai
+
+            uniqueIds.Remove(2);
+
+            Console.WriteLine("Hashset pasalinus elementa:");
+
+            foreach  (int id  in uniqueIds)
+            {
+                Console.WriteLine($"Id: {id}");
+            }
+
+            bool hasNumber2 = uniqueIds.Contains(2);
+
+            Console.WriteLine($"ar rinkinys turi skaiciu 2? atsakymas {hasNumber2}");
+
+            bool hasNumber3 = uniqueIds.Contains(3);
+
+            Console.WriteLine($"ar rinkinys turi skaiciu 3? atsakymas {hasNumber3}");
+
+            uniqueIds.Clear();
+            Console.WriteLine("Rinkinys po isvalymo:");
+
+            foreach (int id in uniqueIds)
+            {
+                Console.WriteLine($"id: {id}");
+            }
+
+            List<int> idsList = uniqueIds.ToList();
+        } 
+    }
+}
