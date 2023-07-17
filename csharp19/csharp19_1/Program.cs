98e8775798a9fd67a44fd782230350681496338e
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-17 23:09:33 +0300

    csharp19

---
+++ csharp19/csharp19_1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace csharp19_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Užduotis 1: Sukurti HashSet, kuris saugo sveikųjų skaičių seką. 
+            // Pridėti 5 skaičius ir ištrinti vieną.
+
+            HashSet<int> numbers = new HashSet<int>();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                numbers.Add(i);
+            }
+
+            numbers.Remove(3);
+        }
+    }
+}
---
+++ csharp19/csharp19_3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace csharp19_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Užduotis 3: Sukurti HashSet su šalių pavadinimais ir atspausdinti jų kiekį.
+
+            HashSet<string> countries = new HashSet<string>()
+            {
+                "Lithuania",
+                "Mexico",
+                "Italy",
+                "Columbia",
+                "Germany",
+            };
+
+            Console.WriteLine($"Saliu kiekis: {countries.Count}");
+        }
+    }
+}
