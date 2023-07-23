40e8d9441801ba92b4a817619d7af3959dfec8e8
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-23 16:22:29 +0300

    csharp26_2

--- csharp26/csharp26.Tests/PrimeGeneratorTests.cs
+++ csharp26/csharp26.Tests/PrimeGeneratorTests.cs
@@ -1,39 +1,36 @@
 using csharp26_1;
 
-namespace csharp26.Tests
+namespace csharp26_1.Tests
 {
-    namespace csharp26.Tests
+    [TestClass]
+    public class PrimeGeneratorTests
     {
-        [TestClass]
-        public class PrimeGeneratorTests
+        [TestMethod]
+        public void CallingGeneratePrimesWith10n()
         {
-            [TestMethod]
-            public void CallingGeneratePrimesWith10n()
-            {
-                // Arrange
-                int n = 10;
-                List<int> expectedResult = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+            // Arrange
+            int n = 10;
+            List<int> expectedResult = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
-                // Act
-                List<int> actualResult = PrimeGenerator.GeneratePrimes(n);
+            // Act
+            List<int> actualResult = PrimeGenerator.GeneratePrimes(n);
 
-                // Assert
-                CollectionAssert.AreEqual(expectedResult, actualResult); // ColletionAssert. padeda su List testu.
-            }
+            // Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult); // ColletionAssert. padeda su List testu.
+        }
 
-            [TestMethod]
-            public void CallingGeneratePrimesWith20n()
-            {
-                // Arrange
-                int n = 20;
-                List<int> expectedResult = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 };
+        [TestMethod]
+        public void CallingGeneratePrimesWith20n()
+        {
+            // Arrange
+            int n = 20;
+            List<int> expectedResult = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 };
 
-                // Act
-                List<int> actualResult = PrimeGenerator.GeneratePrimes(n);
+            // Act
+            List<int> actualResult = PrimeGenerator.GeneratePrimes(n);
 
-                // Assert
-                CollectionAssert.AreEqual(expectedResult, actualResult); // ColletionAssert. padeda su List testu.
-            }
+            // Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult); // ColletionAssert. padeda su List testu.
         }
     }
 }
