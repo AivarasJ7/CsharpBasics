c87f0e6f733a77ad3ee8e5aacdf82482f37e06b4
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-26 13:40:02 +0300

    fix

--- csharp27/csharp27/Calculator.cs
+++ csharp27/csharp27/Calculator.cs
@@ -10,7 +10,7 @@
         /// 1.Tikriname ar sarasas yra "null" arba tuščias, rezultatas yra 0
         /// 2. Apskaiciuojame visu pateikto saraso lyginiu skaiciu suma
         /// 2.1 Susikuriame "lyginiu skaiciu suma = 0" kintamaji kad nuo starto butu 0.
-        /// 2.2 tikriname ar skaicius is saraso dalinasi is 2 ir po dalybos yra lygus 0
+        /// 2.2 tikriname ar skaicius is saraso dalinasi is 2 ir po modalines dalybos yra lygus 0
         /// 2.3 pridedame ta lygini skaiciu kuris dalinasi is 2 i bendra lyginiu skaiciu suma.
         /// </summary>
         /// <param name="numbers"></param>
