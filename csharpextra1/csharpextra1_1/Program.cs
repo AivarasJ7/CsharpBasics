4de64b389eb108c3c6f91f5e04659bbd998eb415
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-07-31 13:09:57 +0300

    csharpextra1_1

---
+++ csharpextra1/csharpextra1_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace csharpextra1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Standartiškai: Šiam assignment - vienas solution. Kiekviena užduotis tai atskiras projektas.
+            // Po pirmos užduoties jau galit padaryti commit ir push, sukurti merge request,
+            // o visas kitas pridėsite atskiru commit ir push.
+
+            // 1.Liepkite vartotojui įvesti bet kokį tekstą.
+            // Atspausdinkite ekrane žodžių, kurie prasideda raide "a" arba "A", skaičių.
+
+            Console.WriteLine("Iveskite teksta:");
+            string text = Console.ReadLine();
+
+            string[] words = text.Split(' ');
+
+            int count = 0;
+
+            foreach (string word in words)
+            {
+                if (word.StartsWith("a", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(word);
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Skaicius zodziu, kurie prasideda raide 'a' arba 'A': {count}");
+        }
+    }
+}
