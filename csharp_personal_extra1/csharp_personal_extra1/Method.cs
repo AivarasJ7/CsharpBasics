a4a6e38b194f70dd5d0c06d116a6a88773228a4e
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-08-02 00:07:36 +0300

    fix

--- csharp_personal_extra1/csharp_personal_extra1/Method.cs
+++ csharp_personal_extra1/csharp_personal_extra1/Method.cs
@@ -31,13 +31,22 @@
             static string FizzBuzz(int number)
             {
                 if (number % 15 == 0)
+                {
                     return "FizzBuzz";
+                }
                 else if (number % 3 == 0)
+                {
                     return "Fizz";
+                }
+                   
                 else if (number % 5 == 0)
+                {
                     return "Buzz";
+                }
                 else
+                {
                     return number.ToString();
+                }
             }
 
             string[] resultArray = new string[array.Length];
