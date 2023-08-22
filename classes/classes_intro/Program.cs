8c32a349597cccd05acd1b37dfb954d7184cf0c4
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-08-22 22:12:14 +0300

    po paskaitos

---
+++ classes/classes_intro/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace classes_intro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.PrintHey(); //metodas kuris priklauso klasei
+
+            Program programObject = new Program();
+
+            // methodas kuris priklauso objektui
+            programObject.PrintHello();
+        }
+
+        internal static void PrintHey() // static priklauso klasei
+        {
+            Console.WriteLine("Hello");
+        }
+
+        internal void PrintHello() // non static priklauso objektui sukurtam klasei
+        {
+            Console.WriteLine("Hello");
+        }
+           
+
+    }
+}
