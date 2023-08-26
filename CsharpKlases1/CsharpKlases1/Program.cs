4032b9d8561582bda17463f4ac7b67c9920005c5
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-08-26 17:05:38 +0300

    CsharpKlases1

---
+++ CsharpKlases1/CsharpKlases1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace CsharpKlases1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Sukurkite klasę studento informacijai saugoti.
+            // Savybės, kurias turi studentas tai Vardas, Pavardė, Amžius, Lytis.
+            // Sugalvokite dar dvi sąvybes.
+            // Studento klasė turi metodą Prisistatyti.Iškvietus metodą, ekrane išspausdinamas tekstas: "Aš Jonas Jonaitis. Esu vyras. Man 28."
+            // Savybės ir metodai anglų kalba.
+
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                Console.WriteLine("Uzregistruoti Studenta:");
+                Student student = new Student();
+
+                Console.Write("Iveskite studento varda:");
+                student.Name= Console.ReadLine();
+
+                Console.Write("Iveskite studento pavarde:");
+                student.Surname = Console.ReadLine();
+
+                Console.Write("Iveskite studento amziu:");
+                student.Age = int.Parse(Console.ReadLine());
+
+                Console.Write("Iveskite studento lyti:");
+                student.Gender = Console.ReadLine();
+
+                Console.Write("Iveskite studento ugi (cm):");
+                student.Height = int.Parse(Console.ReadLine());
+
+                Console.Write("Iveskite studento svori (kg):");
+                student.Weight = int.Parse(Console.ReadLine());
+
+                students.Add(student);
+
+                Console.WriteLine("Ar norite testi t/n?");
+                string choice = Console.ReadLine();
+
+                if (choice != "t")
+                {
+                    break;
+                }
+            }
+
+            foreach (Student student in students)
+            {
+                student.MakeAGreeting();
+            }
+        }
+    }
+}
---
+++ CsharpKlases1/CsharpKlases1/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace CsharpKlases1
+{
+    public class Student
+    {
+        // Sukurkite klasę studento informacijai saugoti.
+        // Savybės, kurias turi studentas tai Vardas, Pavardė, Amžius, Lytis.
+        // Sugalvokite dar dvi sąvybes.
+        // Studento klasė turi metodą Prisistatyti.Iškvietus metodą, ekrane išspausdinamas tekstas: "Aš Jonas Jonaitis. Esu vyras. Man 28."
+        // Savybės ir metodai anglų kalba.
+
+        //savybes - properties (public)
+
+        public string Name;
+        public string Surname;
+        public int Age;
+        public string Gender;
+        public int Height;
+        public int Weight;
+
+        //metodai - (public)
+        public void MakeAGreeting()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"As {Name} {Surname}. Esu {Gender}. Man yra {Age} metai-(u), " +
+                $"mano ugis yra {Height}cm, o svoris {Weight}kg.");
+        }
+
+    }
+}
