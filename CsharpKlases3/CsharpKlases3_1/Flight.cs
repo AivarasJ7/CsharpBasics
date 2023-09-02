e2b8c937bdc989d2426290cb90b879cd0ce9de6a
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-02 23:18:05 +0300

    CsharpKlases3

--- CsharpKlases3/CsharpKlases3_1/Flight.cs
+++ CsharpKlases3/CsharpKlases3_1/Flight.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace CsharpKlases3_1
+﻿namespace CsharpKlases3_1
 {
     public class Flight
     {
--- CsharpKlases3/CsharpKlases3_1/Program.cs
+++ CsharpKlases3/CsharpKlases3_1/Program.cs
@@ -1,10 +1,48 @@
 ﻿namespace CsharpKlases3_1
 {
-    internal class Program
+    public class Program
     {
+        // 2. Sukurkite metodą ar metodus kurie nuskaito iš failo duomenis ir užpildo skrydžią sąrašą (List).
+        // Failą sugalvokite patys pagal duotą klasės aprašymą. faile turi būti bent keli skrydžiai. 
+        // Galutinis rezultatas šio punkto turi būti List<Flight> užpildytas skrydžiais.
+
         static void Main(string[] args)
         {
+            List<Flight> flights = ReadFlightsFromFile("skrydziai.txt");
 
+            foreach (Flight flight in flights)
+            {
+                Console.WriteLine(flight.GetFlightInfo());
+                Console.WriteLine();
+            }
+        }
+
+        public static List<Flight> ReadFlightsFromFile(string fileName)
+        {
+            List<Flight> flights = new List<Flight>();
+
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(' ');
+
+                        Flight flight = new Flight
+                        {
+                            DepartureCity = data[0],
+                            ArrivalCity = data[1],
+                            DepartureDateTime = DateTime.Parse($"{data[2]} {data[3]}"),
+                            ArrivalDateTime = DateTime.Parse($"{data[4]} {data[5]}"),
+                            Price = decimal.Parse(data[6]),
+                            Currency = data[7],
+                            FlightNumber = data[8],
+                            Class = data[9]
+                        };
+
+                        flights.Add(flight);
+                    }
+                }
+            }
         }
     }
-}
