4c307822f1bcbe6230a20bcb8202796c3744fabf
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-19 16:15:47 +0300

    CsharpKlases7

---
+++ CSharpKlases7/CSharpKlases7/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace CSharpKlases7
+{
+    public class Book
+    {
+        // Knygų klasės: Sukurkite pagrindinę Book klasę su savybėmis Title, Author ir PublicationYear ir metodu PrintBookInfo(),
+        // kuris išveda visą informaciją apie knygą.
+        public Book(string title, string author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+
+        public string Title { get; }
+        public string Author { get; }
+        public int PublicationYear { get; }
+
+        public string PrintBookInfo()
+        {
+            return $"Knygos pavadinimas yra {Title}, jos autorius yra {Author} ir ji buvo pagaminta {PublicationYear} metais.";
+        }
+    }
+}
---
+++ CSharpKlases7/CSharpKlases7/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace CSharpKlases7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Knygų klasės: Sukurkite pagrindinę Book klasę su savybėmis Title, Author ir PublicationYear ir metodu PrintBookInfo(),
+            // kuris išveda visą informaciją apie knygą.
+            // Tada sukurkite paveldėtą Textbook klasę su papildoma savybe Subject ir papildomu metodu PrintTextbookInfo(), kuris išveda informaciją apie vadovėlį.
+            // Savo nuožiūra galite sugalvoti naujų paveldinčių klasių ir sąvybių.
+            // Sukurkite paprastą programą, kurioje vartotojas galėtų įvesti duomenis ir, pagal situaciją, būtų sukurta viena ar kita klasė,
+            // o duomenų suvedimo metu būtų pildomas bazinės klasės sąrašas.
+            // Baigus įvedinėti programa išspausdina visų objektų informaciją.
+
+            List<Book> books = new List<Book>();
+
+            Console.WriteLine("Sveiki, atvyke!");
+
+            while (true)
+            {
+                Console.WriteLine("Pasirinkite, ką darysite:");
+                Console.WriteLine("1 - Prideti nauja knyga");
+                Console.WriteLine("2 - Baigti");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddNewBook(books);
+                        break;
+
+                    case 2:
+                        DisplayBookInfo(books);
+                        return;
+
+                    default:
+                        Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
+                        break;
+                }
+            }
+        }
+
+        static void AddNewBook(List<Book> books)
+        {
+            Console.WriteLine("Iveskite knygos pavadinima: ");
+            string title = Console.ReadLine();
+            Console.WriteLine("Iveskite knygos autoriu: ");
+            string author = Console.ReadLine();
+            Console.WriteLine("Iveskite knygos isleidimo metus: ");
+            int publicationYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ar tai yra vadovelis? (t/n): ");
+            string isTextbookInput = Console.ReadLine().ToLower();
+
+            if (isTextbookInput == "t")
+            {
+                Console.WriteLine("Iveskite vadovelio tema: ");
+                string subject = Console.ReadLine();
+                Textbook newTextbook = new Textbook(title, author, publicationYear, subject);
+                books.Add(newTextbook);
+            }
+            else if (isTextbookInput == "n")
+            {
+                Book newBook = new Book(title, author, publicationYear);
+                books.Add(newBook);
+            }
+            else
+            {
+                Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
+            }
+        }
+
+        static void DisplayBookInfo(List<Book> books)
+        {
+            Console.WriteLine("Informacija apie knygas:");
+            foreach (var book in books)
+            {
+                if (book is Textbook textbook)
+                {
+                    Console.WriteLine(textbook.PrintTextbookInfo());
+                }
+                else
+                {
+                    Console.WriteLine(book.PrintBookInfo());
+                }
+            }
+        }
+    }
+}
