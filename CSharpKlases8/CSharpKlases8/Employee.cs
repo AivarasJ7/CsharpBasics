8950a2c50b88b3087c98a27c686c6b2f0f037080
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-19 18:03:15 +0300

    CSharpKlases8

---
+++ CSharpKlases8/CSharpKlases8/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpKlases8
+{
+    public class Employee
+    {
+        // Darbuotojų klasės: Sukurkite bazinę klasę Employee su savybėmis Name, JobTitle ir Salary ir metodu PrintEmployeeInfo(),
+        // kuris išveda visą informaciją apie darbuotoją.
+
+        public Employee(string name, string jobTitle, decimal salary)
+        {
+            Name = name;
+            JobTitle = jobTitle;
+            Salary = salary;
+        }
+
+        public string Name { get; }
+        public string JobTitle { get; }
+        public decimal Salary { get; }
+
+        public string PrintEmployeeInfo()
+        {
+            return $"Darbuotojo vardas yra {Name}, jo pareigos yra {JobTitle} ir jo atlyginimas yra {Salary}.";
+        }
+    }
+}
---
+++ CSharpKlases8/CSharpKlases8/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace CSharpKlases8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Darbuotojų klasės: Sukurkite bazinę klasę Employee su savybėmis Name, JobTitle ir Salary ir metodu PrintEmployeeInfo(),
+            // kuris išveda visą informaciją apie darbuotoją.
+            // Tada sukurkite paveldėtą klasę Manager su papildoma savybe NumberOfSubordinates ir papildomu metodu PrintManagerInfo(),
+            // kuris išveda informaciją apie vadovą. Savo nuožiūra galite sugalvoti naujų paveldinčių klasių ir sąvybių.
+            // Sukurkite paprastą programą, kurioje vartotojas galėtų įvesti duomenis ir, pagal situaciją, būtų sukurta viena ar kita klasė,
+            // o duomenų suvedimo metu būtų pildomas bazinės klasės sąrašas.
+            // Baigus įvedinėti programa išspausdina visų objektų informaciją.
+
+            List<Employee> employees = new List<Employee>();
+
+            Console.WriteLine("Sveiki!");
+
+            while (true)
+            {
+                Console.WriteLine("Pasirinkite veiksma:");
+                Console.WriteLine("1 - Prideti nauja darbuotoja");
+                Console.WriteLine("2 - Prideti nauja vadova");
+                Console.WriteLine("3 - Rodyti darbuotoju ir vadovu informacija");
+                Console.WriteLine("4 - Iseiti");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddNewEmployee(employees);
+                        break;
+
+                    case 2:
+                        AddNewManager(employees);
+                        break;
+
+                    case 3:
+                        DisplayEmployeeAndManagerInfo(employees);
+                        break;
+
+                    case 4:
+                        return;
+
+                    default:
+                        Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
+                        break;
+                }
+            }
+        }
+
+        static void AddNewEmployee(List<Employee> employees)
+        {
+            Console.WriteLine("Iveskite darbuotojo varda:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Iveskite darbuotojo pareigas:");
+            string jobTitle = Console.ReadLine();
+            Console.WriteLine("Iveskite darbuotojo atlyginima:");
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            Employee newEmployee = new Employee(name, jobTitle, salary);
+            employees.Add(newEmployee);
+        }
+
+        static void AddNewManager(List<Employee> employees)
+        {
+            Console.WriteLine("Iveskite vadovo varda:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Iveskite vadovo pareigas:");
+            string jobTitle = Console.ReadLine();
+            Console.WriteLine("Iveskite vadovo atlyginima:");
+            decimal salary = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Iveskite pavaldiniu skaičiu:");
+            int numberOfSubordinates = int.Parse(Console.ReadLine());
+
+            Manager newManager = new Manager(name, jobTitle, salary, numberOfSubordinates);
+            employees.Add(newManager);
+        }
+
+        static void DisplayEmployeeAndManagerInfo(List<Employee> employees)
+        {
+            Console.WriteLine("Darbuotoju ir vadovu Informacija:");
+            foreach (var employee in employees)
+            {
+                if (employee is Manager manager)
+                {
+                    Console.WriteLine(manager.PrintManagerInfo());
+                }
+                else
+                {
+                    Console.WriteLine(employee.PrintEmployeeInfo());
+                }
+            }
+        }
+    }
+}
