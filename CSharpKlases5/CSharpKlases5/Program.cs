1839b6cc22f5a47b8de3bb35ac846ca92bfc7fd2
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-19 13:12:04 +0300

    CsharpKlases5

--- CSharpKlases5/CSharpKlases5/Program.cs
+++ CSharpKlases5/CSharpKlases5/Program.cs
@@ -29,54 +29,62 @@
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("Iveskite transporto priemones marke: ");
-                        string brand = Console.ReadLine();
-                        Console.WriteLine("Iveskite transporto priemones modeli: ");
-                        string model = Console.ReadLine();
-                        Console.WriteLine("Iveskite transporto priemones pagaminimo metus: ");
-                        int year = int.Parse(Console.ReadLine());
-
-                        Console.WriteLine("Ar tai yra automobilis? (t/n): ");
-                        string isCarInput = Console.ReadLine().ToLower();
-
-                        if (isCarInput == "t")
-                        {
-                            Console.WriteLine("Iveskite automobilio sedimu vietu skaiciu: ");
-                            int numberOfSeats = int.Parse(Console.ReadLine());
-                            Car newCar = new Car(brand, model, year, numberOfSeats);
-                            vehicles.Add(newCar);
-                        }
-                        else if (isCarInput == "n")
-                        {
-                            Vehicle newVehicle = new Vehicle(brand, model, year);
-                            vehicles.Add(newVehicle);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
-                        }
+                        AddNewVehicle(vehicles);
                         break;
 
                     case 2:
-                        Console.WriteLine("Informacija apie transporto priemones:");
-                        foreach (var vehicle in vehicles)
-                        {
-                            if (vehicle is Car car)
-                            {
-                                Console.WriteLine(car.PrintCarInfo());
-                            }
-                            else
-                            {
-                                Console.WriteLine(vehicle.PrintInfo());
-                            }
-                        }
+                        DisplayVehicleInfo(vehicles);
                         return;
 
                     default:
-                        {
-                            Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar kartą.");
-                        }
+                        Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
                         break;
+                }
+            }
+        }
+
+        static void AddNewVehicle(List<Vehicle> vehicles)
+        {
+            Console.WriteLine("Iveskite transporto priemones marke: ");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Iveskite transporto priemones modeli: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Iveskite transporto priemones pagaminimo metus: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ar tai yra automobilis? (t/n): ");
+            string isCarInput = Console.ReadLine().ToLower();
+
+            if (isCarInput == "t")
+            {
+                Console.WriteLine("Iveskite automobilio sedimu vietu skaiciu: ");
+                int numberOfSeats = int.Parse(Console.ReadLine());
+                Car newCar = new Car(brand, model, year, numberOfSeats);
+                vehicles.Add(newCar);
+            }
+            else if (isCarInput == "n")
+            {
+                Vehicle newVehicle = new Vehicle(brand, model, year);
+                vehicles.Add(newVehicle);
+            }
+            else
+            {
+                Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
+            }
+        }
+
+        static void DisplayVehicleInfo(List<Vehicle> vehicles)
+        {
+            Console.WriteLine("Informacija apie transporto priemones:");
+            foreach (var vehicle in vehicles)
+            {
+                if (vehicle is Car car)
+                {
+                    Console.WriteLine(car.PrintCarInfo());
+                }
+                else
+                {
+                    Console.WriteLine(vehicle.PrintInfo());
                 }
             }
         }
--- CSharpKlases5/CSharpKlases5/Vehicle.cs
+++ CSharpKlases5/CSharpKlases5/Vehicle.cs
@@ -4,13 +4,11 @@
     {
         // Transporto priemonių klasės: Sukurkite pagrindinę Vehicle klasę su savybėmis Brand, Model ir Year ir metodu PrintInfo(),
         // kuris išveda visą informaciją apie transporto priemonę.
-
         public Vehicle(string brand, string model, int year)
         {
             Brand = brand;
             Model = model;
             Year = year;
-            
         }
 
         public string Brand { get; }
@@ -21,8 +19,5 @@
         {
             return $"Transporto priemones marke yra {Brand}, jos modelis yra {Model} ir ji buvo pagaminta {Year} metais.";
         }
-
-
-
     }
 }
