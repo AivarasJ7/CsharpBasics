31349ac7386ecc579221562c3a84620cb1494192
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-25 14:50:07 +0300

    CSharpKlases13

---
+++ CSharpKlases13/CSharpKlases13/Physicist.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpKlases13
+{
+    public class Physicist : Scientist
+    {
+        // Tada sukurkite paveldėtą klasę Physicist su papildoma savybe Specialization ir papildomu metodu PrintPhysicistInfo(),
+        // kuris išveda informaciją apie fiziką. Savo nuožiūra galite sugalvoti naujų paveldinčių klasių ir sąvybių.
+
+        public Physicist(string name, string fieldOfStudy, string specialization) : base(name, fieldOfStudy)
+        {
+            Specialization = specialization;
+        }
+
+        public string Specialization { get; }
+
+        public void PrintPhysicistInfo()
+        {
+            Console.WriteLine($"Vardas: {Name}, studiju srytis: {FieldOfStudy}, specilizacija {Specialization}");
+        }
+    }
+}
