57d861a689d83d05fd0ba2b03f7f59f4a9eca681
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-28 18:22:44 +0300

    CSharpKlases14

---
+++ CSharpKlases14/CSharpKlases14/Plant.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpKlases14
+{
+    public class Plant
+    {
+        // Augalų klasės: Sukurkite bazinę klasę Plant su savybėmis Species ir Height ir metodu PrintPlantInfo(), kuris išveda visą informaciją apie augalą.
+
+        public Plant(string species, int height)
+        {
+            Species = species;
+            Height = height;
+        }
+
+        public string Species { get; }
+        public int Height { get; }
+
+        public string PrintPlantInfo()
+        {
+            return $"Augalo rusis: {Species}, aukstis: {Height} cm.";
+        }
+    }
+}
---
+++ CSharpKlases14/CSharpKlases14/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace CSharpKlases14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Augalų klasės: Sukurkite bazinę klasę Plant su savybėmis Species ir Height ir metodu PrintPlantInfo(), kuris išveda visą informaciją apie augalą.
+            // Tada sukurkite paveldėtą klasę Flower su papildoma savybe Color ir papildomu metodu PrintFlowerInfo(), kuris išveda informaciją apie gėlę.
+            // Savo nuožiūra galite sugalvoti naujų paveldinčių klasių ir sąvybių.
+
+            // Sukurkite paprastą programą, kurioje vartotojas galėtų įvesti duomenis ir, pagal situaciją, būtų sukurta viena ar kita klasė, o duomenų suvedimo metu būtų pildomas bazinės klasės sąrašas.
+            // Baigus įvedinėti programa išspausdina visų objektų informaciją
+
+            List<Plant> plants = new List<Plant>();
+
+            Console.WriteLine("Sveiki!");
+
+            while (true)
+            {
+                Console.WriteLine("Pasirinkite, ką darysite:");
+                Console.WriteLine("1 - Prideti nauja augala");
+                Console.WriteLine("2 - Prideti nauja gele");
+                Console.WriteLine("3 - Baigti");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Plant newPlant = AddNewPlant();
+                        plants.Add(newPlant);
+                        break;
+
+                    case 2:
+                        Flower newFlower = AddNewFlower();
+                        plants.Add(newFlower);
+                        break;
+
+                    case 3:
+                        DisplayPlantInfo(plants);
+                        return;
+
+                    default:
+                        Console.WriteLine("Neteisingas pasirinkimas. Bandykite dar karta.");
+                        break;
+                }
+            }
+        }
+
+        private static Plant AddNewPlant()
+        {
+            Console.WriteLine("Iveskite augalo rusi: ");
+            string species = Console.ReadLine();
+
+            Console.WriteLine("ĮIveskite augalo auksti (cm): ");
+            int height = int.Parse(Console.ReadLine());
+
+            return new Plant(species, height);
+        }
+
+        private static Flower AddNewFlower()
+        {
+            Console.WriteLine("Iveskite geles rusi: ");
+            string species = Console.ReadLine();
+
+            Console.WriteLine("Iveskite geles auksti (cm): ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Iveskite geles spalva: ");
+            string color = Console.ReadLine();
+
+            return new Flower(species, height, color);
+        }
+
+        private static void DisplayPlantInfo(List<Plant> plants)
+        {
+            Console.WriteLine("Informacija apie augalus ir geles:");
+            foreach (Plant plant in plants)
+            {
+                if (plant is Flower flower)
+                {
+                    Console.WriteLine(flower.PrintFlowerInfo());
+                }
+                else
+                {
+                    Console.WriteLine(plant.PrintPlantInfo());
+                }
+            }
+        }
+    }
+}
