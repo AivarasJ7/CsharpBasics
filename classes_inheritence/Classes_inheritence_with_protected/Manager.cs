816403e069b374f0a4a30e0c59b7fd345a0c6013
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-10-10 19:56:03 +0300

    po pamokos

---
+++ classes_inheritence/Classes_inheritence_with_protected/Manager.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes_inheritence_with_protected
+{
+    public class Manager : Employee
+    {
+        public Manager(string name, string description) : base(name, description)
+        {
+
+        }
+
+        // dynaminis polimorfizmas
+        public override string GetRoleDescription()
+        {
+            return $"Roles aprasymas is Manager klases. {Name}. {Description}.";
+        }
+    }
+}
